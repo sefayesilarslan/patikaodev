5875ebeafec4fbb5d12c5233bebc774dc1a54762
Sefa <sefayesilarslan1922@gmail.com>
2023-09-11 15:37:20 +0300

    patikaödevleri

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace donguler{
+
+
+class program{
+    //ekrandan girilen sayıya kadar olan tek sayıları yazan program
+    static void Main(string[] args){
+    
+    Console.WriteLine("Bir sayı girin");
+    int sayac=int.Parse(Console.ReadLine());
+
+    for(int i =1; i <= sayac; i++){
+        if(i%2==1){
+            Console.WriteLine(i);
+        }
+    }
+
+
+    int sayac2=1000;
+    int toplam=0;
+    
+    for (int i = 1; i <= sayac2; i++)
+    {
+        if(i%2==0){
+            toplam+=i;
+        }
+    }
+
+    Console.WriteLine("Toplam değer:" + toplam);
+
+    }
+}
+
+
+}
+
